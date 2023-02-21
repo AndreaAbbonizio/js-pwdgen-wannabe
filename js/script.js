428fc1e8fc14f263cd32f98c7199f0b8b6de23fa
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-02-21 14:22:34 +0100

    Exercise finished.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+
+
+
+let nomeUtente = prompt("Come ti chiami?");
+
+console.log(nomeUtente);
+
+let cognomeUtente = prompt("Qual'è il tuo cognome?");
+
+console.log(cognomeUtente);
+
+let coloreUtente = prompt("Qual'è il tuo colore preferito?");
+
+console.log(coloreUtente);
+
+const numeroUtente = "23";
+
+console.log(numeroUtente);
+
+document.write(nomeUtente + cognomeUtente + coloreUtente + numeroUtente);
+
+
+
+
+
